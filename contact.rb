e3aeba0087aaa23a364e4cd78edc81481ef43e0f
Xayaseth Boudsady <boudsadyx@gmail.com>
2018-07-08 19:34:28 -0700

    Added PhoneNumber class

--- contact.rb
+++ contact.rb
@@ -13,6 +13,22 @@ class Contact
         @last_name
     end
 
+    def last_first
+        last_first = last_name
+        last_first += ", "
+        last_first += first_name
+        if !@middle_name.nil?
+            last_first += " "
+            last_first += middle_name(0, 1)
+            last_first += "."
+        end
+        last_first
+    end
+
+    def first_last
+        first_name + " " + last_name
+    end
+
     def full_name
         full_name = first_name
         if !@middle_name.nil?
@@ -23,14 +39,33 @@ class Contact
         full_name += last_name
         full_name
     end
+
+    def to_s(format = 'full_name')
+        case format
+            when 'full_name'
+                full_name
+            when 'last_first'
+                last_first
+            when 'first'
+                first_name
+            when 'last'
+                last_name
+            else
+                first_name
+        end
+    end
+
 end
 
 jason = Contact.new
 jason.first_name = "Jason"
 jason.last_name = "Seifer"
+puts jason.to_s
+puts jason.to_s('full_name')
+puts jason.to_s('last_first')
 
 nick = Contact.new
 nick.first_name = "Nick"
 nick.middle_name = "A"
 nick.last_name = "Pettit"
-puts nick.full_name
+puts nick.to_s('first_last')
