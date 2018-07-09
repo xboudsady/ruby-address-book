3b96b198efa502980435c63873bf3d60022c8bbb
Xayaseth Boudsady <boudsadyx@gmail.com>
2018-07-08 19:09:24 -0700

    Created Contact class

---
+++ contact.rb
@@ -0,0 +1,36 @@
+class Contact
+    attr_writer :first_name, :middle_name, :last_name
+
+    def first_name
+        @first_name
+    end
+
+    def middle_name
+        @middle_name
+    end
+
+    def last_name
+        @last_name
+    end
+
+    def full_name
+        full_name = first_name
+        if !@middle_name.nil?
+            full_name += " "
+            full_name += middle_name
+        end
+        full_name += " "
+        full_name += last_name
+        full_name
+    end
+end
+
+jason = Contact.new
+jason.first_name = "Jason"
+jason.last_name = "Seifer"
+
+nick = Contact.new
+nick.first_name = "Nick"
+nick.middle_name = "A"
+nick.last_name = "Pettit"
+puts nick.full_name
