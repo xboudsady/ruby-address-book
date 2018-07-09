de3f5106960fdedca01fa0aede1fb61ccc9bf9b6
Xayaseth Boudsady <boudsadyx@gmail.com>
2018-07-09 15:44:26 -0700

    Added search functionality for name, address, and phone number.

--- address_book.rb
+++ address_book.rb
@@ -7,12 +7,64 @@ class AddressBook
         @contacts = []
     end
 
+    def find_by_name(name)
+        results = []
+        search = name.downcase
+        contacts.each do |contact|
+            if contact.full_name.downcase.include?(search) || contact.last_name.downcase.include?(search)
+                results.push(contact)
+            end
+        end
+        puts "Name search results (#{search})"
+        results.each do |contact|
+            puts contact.to_s('full_name')
+            contact.print_addresses
+            puts "\n"
+        end
+    end
+    
+    def find_by_phone_number(number)
+        results = []
+        search = number.gsub("-", "")               # .gsub() replaces any dashes with a nothing
+        contacts.each do |contact|
+            contact.phone_numbers.each do |phone_number|
+                if phone_number.number.gsub("-", "").include?(search)
+                    results.push(contact)
+                end
+            end
+        end
+        print_results("Phone search results (#{search})", results)
+    end
+
+    def find_by_address(query)
+        results = []
+        search = query.downcase
+        contacts.each do |contact|
+            contact.addresses.each do |address|
+                if address.to_s('long').downcase.include?(search)
+                    results.push(contact) unless results.include?(contact)
+                end
+            end
+        end
+        print_results("Adress search results (#{search}", results)
+    end
+
     def print_contact_list
         puts "Contact List"
         contacts.each do |contact|
             puts contact.to_s('last_first')
         end
     end
+
+    def print_results(search, results)
+        puts search
+        results.each do |contact|
+            puts contact.to_s('full_name')
+            contact.print_phone_numbers
+            contact.print_addresses
+            puts "\n"
+    end
+end
 end
 
 address_book = AddressBook.new
@@ -33,4 +85,10 @@ nick.add_address("Home", "222 Two Lane", "", "Portland", "OR", "12345")
 address_book.contacts.push(jason)
 address_book.contacts.push(nick)
 
-address_book.print_contact_list
+#address_book.print_contact_list
+
+#address_book.find_by_name('e')
+
+#address_book.find_by_name("e")
+#address_book.find_by_phone_number("2")
+address_book.find_by_address("two")
